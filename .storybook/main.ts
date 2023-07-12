6892d53d4705d72241f7bdd2256211bfaacc3a62
masakinihirota <masakinihirota@gmail.com>
2023-07-13 00:14:07 +0900

    🎨 update 20 files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from "@storybook/nextjs"
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,10 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-    "plugins": [require('prettier-plugin-tailwindcss')],
-    "semi": false,
-    "singleQuote": true,
-    "printWidth": 80,
-    "tabWidth": 2,
-    "trailingComma": "all",
-    "jsxSingleQuote": true
-  }
+  plugins: [require("prettier-plugin-tailwindcss")],
+  semi: false,
+  singleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "all",
+  jsxSingleQuote: true,
+}
