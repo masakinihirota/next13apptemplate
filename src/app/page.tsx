169dbf190a37bc2f513269a46d8d6e080e3ea178
masakinihirota <masakinihirota@gmail.com>
2023-07-24 22:12:27 +0900

    🎨 update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { type NextPage } from "next"
-import Link from "next/link"
+import { type NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -10,9 +10,9 @@ const Home: NextPage = () => {
       Next.js app router 開発用テンプレート (Storybook Supabase shadcn/ui)
       <br />
       {/* ↓この行を消したり復元するとimport文が自動削除、自動挿入サれるのが確認できます。 */}
-      <Link href='/'>Home</Link>
+      <Link href="/">Home</Link>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
