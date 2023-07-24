9f57e7cbedb4563658409eec230f8fe1e9c78ca9
masakinihirota <masakinihirota@gmail.com>
2023-07-24 15:58:44 +0900

    🎨 update page.tsx and tsconfig.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-export default function Home() {
+import { type NextPage } from "next"
+import Link from "next/link"
+
+const Home: NextPage = () => {
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
   return (
     <main>
       <h1>VNS.BLUE</h1>
       Next.js app router 開発用テンプレート (Storybook Supabase shadcn/ui)
+      <br />
+      {/* ↓この行を消したり復元するとimport文が自動削除、自動挿入サれるのが確認できます。 */}
+      <Link href='/'>Home</Link>
     </main>
   )
 }
+
+export default Home
