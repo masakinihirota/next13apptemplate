4e7649cd61b3bdb27d6ff794a59eeab53df4d58e
masakinihirota <masakinihirota@gmail.com>
2023-07-25 13:37:46 +0900

    ✨ pnpm instale

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,12 @@
 import type { StorybookConfig } from "@storybook/nextjs"
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-coverage",
   ],
   framework: {
     name: "@storybook/nextjs",
